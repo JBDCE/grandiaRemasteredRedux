55a1622288c174b1841926660c7e6561413350ce
JBDCE <61785226+JBDCE@users.noreply.github.com>
2020-04-24 22:07:10 +0200

    First successfull patching still graphical glitches in first cd contents

--- generateM_DAT.py
+++ generateM_DAT.py
@@ -2,12 +2,14 @@ REMASTER_MDAT = open("TestFileStructure/original_M_DAT.BIN", 'rb')
 REDUX1_MDAT = open("TestFileStructure/redux1_M_DAT.BIN", 'rb')
 REDUX2_MDAT = open("TestFileStructure/redux2_M_DAT.BIN", 'rb')
 NAMES_TXT = open("TestFileStructure/names.txt", 'r')
+STRINGS_TXT = open("TestFileStructure/original_strings.txt", 'r')
 
 redux1Bytes = []
 redux2Bytes = []
 remasterBytes = []
 
 protectedBytes = []
+namePositions = []
 
 allowedChars = [32]
 for i in range(65, 91):
@@ -19,9 +21,9 @@ for i in range(97, 123):
 def populateByteLists():
     global redux1Bytes, redux2Bytes, remasterBytes, protectedBytes
 
+    # Protect the original Names from changing
     rawBytes = REMASTER_MDAT.read()
     names = NAMES_TXT.readlines()
-
     for name in names:
         name = name.rstrip()
         byteName = bytes(name, encoding='ascii')
@@ -31,6 +33,7 @@ def populateByteLists():
             print(name + " not found in M_DAT")
         else:
             protectedBytes += list(range(startAddress, startAddress+len(name)))
+            namePositions.append(list(range(startAddress, startAddress+len(name))))
 
     remasterBytes = bytearray(rawBytes)
 
@@ -45,7 +48,7 @@ def populateByteLists():
     assert len(redux2Bytes) == len(remasterBytes),\
         "Redux2_MDAT is the wrong length"
 
-    print("Remaster lenght: " + str(len(remasterBytes)))
+    print("M_DAT lenght: " + str(len(remasterBytes)))
 
 
 def performChanges():
@@ -92,7 +95,58 @@ def performChanges():
     return outputBytes
 
 
+def generateStringEntries():
+    stringLines = STRINGS_TXT.readlines()
+
+    originalNames = []
+    reduxNames = []
+
+    for namePos in namePositions:
+        originalName = ''
+        redux1Name = ''
+        redux2Name = ''
+        # Load Names
+        for address in namePos:
+            originalName += chr(remasterBytes[address])
+            redux1Name += chr(redux1Bytes[address])
+            redux2Name += chr(redux2Bytes[address])
+        
+        if redux1Name == originalName and redux2Name == originalName:
+            # Do nothing
+            continue
+        elif redux1Name != originalName and redux2Name == originalName:
+            # Take the Redux1 name
+            originalNames.append(originalName)
+            reduxNames.append(redux1Name)
+        elif redux1Name == originalName and redux2Name != originalName:
+            # Take the Redux2 name
+            originalNames.append(originalName)
+            reduxNames.append(redux2Name)
+        elif redux1Name != originalName and redux2Name != originalName and redux1Name != redux2Name:
+            # This should not happen
+            print("This should not have happened")
+    
+    assert len(originalNames) == len(reduxNames), "Names lists unequal"
+    
+    # Search the Strings.txt for the entries
+    newStringLines = []
+
+    for line in stringLines:
+        for index, name in enumerate(originalNames):
+            position = line.rfind(originalNames[index])
+            if position != -1:
+                newStringLines.append(line[:position] + reduxNames[index] + '\n')
+                break
+        else:
+            newStringLines.append(line)
+    
+    with open("TestFileStructure/output_strings.txt", 'w+') as outputFile:
+        outputFile.writelines(newStringLines)
+
+
 populateByteLists()
+
+generateStringEntries()
 
 outputList = performChanges()
 
