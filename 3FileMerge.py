c0930ba691ebd6577aeea6ce7a972484a2177d9b
JBDCE <61785226+JBDCE@users.noreply.github.com>
2021-05-13 15:56:53 +0200

    Added Comparison and Output logic

--- 3FileMerge.py
+++ 3FileMerge.py
@@ -1,13 +1,73 @@
 # Read the cla
+filePathOriginal = ""
+filePath_B = ""
+filePath_C = ""
+filePathOutput = ""
+onConflict = 0
 
-
-# Load the files
-
+# Load the files into bytearrays
+with open(filePathOriginal, 'rb') as A:
+    bytesOriginal = bytearray(A.read())
+with open(filePath_B, 'rb') as B:
+    bytesB = bytearray(B.read())
+with open(filePath_C, 'rb') as C:
+    bytesC = bytearray(C.read())
 
 # Validate they are the same length
+assert len(bytesB) == len(bytesOriginal),\
+     "File B (" + len(bytesB) + ") is the Wrong Length. Should be: " + len(bytesOriginal)
+assert len(bytesC) == len(bytesOriginal),\
+     "File C (" + len(bytesC) + ") is the Wrong Length. Should be: " + len(bytesOriginal)
 
+print("Length Check Successful")
+print("Original File length: " + len(bytesOriginal))
 
 # Perform the changes 
+outputBytes = [b'\x00'] * len(bytesOriginal)
 
+for address in range(len(bytesOriginal)):
+
+    # All three files are the same so no change was made
+    if((bytesOriginal[address] == bytesB[address]) and
+            (bytesOriginal[address] == bytesC[address])):
+        outputBytes[address] = bytesOriginal[address]
+
+    # Take changes from bytesB
+    elif((bytesOriginal[address] != bytesB[address]) and
+            (bytesOriginal[address] == bytesC[address])):
+        outputBytes[address] = bytesB[address]
+
+    # Take changes from bytesC
+    elif((bytesOriginal[address] == bytesB[address]) and
+            (bytesOriginal[address] != bytesC[address])):
+        outputBytes[address] = bytesC[address]
+
+    # Both files differ from the original but both contain the same data
+    elif((bytesOriginal[address] != bytesB[address]) and
+            (bytesOriginal[address] != bytesC[address]) and
+            (bytesB[address] == bytesC[address])):
+        outputBytes[address] = bytesB[address]
+
+    # All 3 files missalign
+    elif((bytesOriginal[address] != bytesB[address]) and
+            (bytesOriginal[address] != bytesC[address]) and
+            (bytesB[address] != bytesC[address])):
+
+        print("All 3 differ from Eachother")
+        print("Address: " + address)
+        print("Original: " + bytesOriginal[address])
+        print("BytesB: " + bytesB[address])
+        print("BytesC: " + bytesC[address])
+        
+        if onConflict==1:
+            outputBytes[address] = bytesOriginal[address]
+        elif onConflict==2:
+            outputBytes[address] = bytesB[address]
+        elif onConflict==3:
+            outputBytes[address] = bytesC[address]
 
 # Write an output File
+with open(filePathOutput, 'wb+') as outputFile:
+    for byte in outputBytes:
+        tmp = byte.to_bytes(1, 'big')
+        outputFile.write(tmp)
